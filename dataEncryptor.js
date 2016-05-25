5517334d8784095249ee73e9c0e3a369dfbb0b42
Nubo Developer <dev@nubosoftware.com>
2016-05-25 15:43:15 +0300

    merge "add encryptConf paramter"
    Change-Id: I612393261738c3c60eee2626db25a97c0e9eedf6

--- dataEncryptor.js
+++ dataEncryptor.js
@@ -1,111 +1,38 @@
 var fs = require('fs');
 var async = require('async');
 
-var modified;
-var encrypt;
-var decrypt;
-var logger;
+function parseParameters(mode, data, parameters, func) {
+    var parsedData = JSON.parse(JSON.stringify(data));
 
-function readFile(fileName, parameters, enc, dec, callback, log) {
-    modified = false;
-    encrypt = enc;
-    decrypt = dec;
-    logger = log;
-
-    var settings;
-    var newSettings;
-    var decryptedSettings;
-
-    async.series([
-        // read file
-        function(callback) {
-            fs.readFile(fileName, function(err, data) {
-                if (err) {
-                    callback(err);
-                    return;
-                }
-
-                var rawSettings = data.toString().replace(/[\n|\t]/g, '');
-                logger.debug("readFile: " + rawSettings);
-                try {
-                    settings = JSON.parse(rawSettings);
-                    callback(null);
-                } catch (err) {
-                    callback(err + ", while parsing " + fileName);
-                }
-            });
-        },
-        // encrypt fields in case some value changed
-        function(callback) {
-            try {
-                newSettings = parseParameters('enc', settings, parameters);
-                callback(null);
-            } catch (err) {
-                callback("encrypting " + err);
-            }
-        },
-        // write file back after encryption
-        function(callback) {
-            if (modified) {
-                var newSettingsToFile = JSON.stringify(newSettings, null, 4);
-                fs.writeFile(fileName, newSettingsToFile, callback);
-            } else {
-                callback(null);
-            }
-        },
-        // decrypt fields
-        function(callback) {
-            try {
-                decryptedSettings = parseParameters('dec', settings, parameters);
-                callback(null);
-            } catch (err) {
-                callback("decrypting " + err);
-            }
-        }
-    ], function(err) {
-
-        if (err) {
-            if (logger)
-                logger.error("readFile: " + err);
-
-            callback(err);
-            return;
-        }
-
-        callback(null, decryptedSettings);
-    });
-}
-
-function parseParameters(mode, data, parameters) {
     for (var k in parameters) {
-        if (typeof data[k] === "object" && data[k] !== null) {
-            if (typeof data[k] !== typeof parameters[k])
+        if (typeof parsedData[k] === "object" && parsedData[k] !== null) {
+            if (typeof parsedData[k] !== typeof parameters[k])
                 throw ("type of parameter " + k + " not same in data and paramters");
 
-            data[k] = parseParameters(mode, data[k], parameters[k]);
+            parsedData[k] = parseParameters(mode, parsedData[k], parameters[k], func);
         } else {
-            if (mode === 'dec' && data[k]) {
-                if (typeof data[k] === "string") {
-                    if (data[k].indexOf("enc:") === 0) {
-                        data[k] = decrypt(data[k]);
+            if (mode === 'dec' && parsedData[k]) {
+                if (typeof parsedData[k] === "string") {
+                    if (parsedData[k].indexOf("enc:") === 0) {
+                        parsedData[k] = func(parsedData[k]);
                     }
-                } else throw ("paramter \"" + k + ": " + data[k] + "\" should be type of string");
+                } else throw ("paramter \"" + k + ": " + parsedData[k] + "\" should be type of string");
 
-            } else if (mode === 'enc' && data[k]) {
-                if (typeof data[k] === "string") {
-                    if (data[k].indexOf("enc:") !== 0) {
-                        data[k] = encrypt(data[k]);
+            } else if (mode === 'enc' && parsedData[k]) {
+                if (typeof parsedData[k] === "string") {
+                    if (parsedData[k].indexOf("enc:") !== 0) {
+                        parsedData[k] = func(parsedData[k]);
                         modified = true;
                     }
-                } else throw ("paramter \"" + k + ": " + data[k] + "\" should be type of string");
+                } else throw ("paramter \"" + k + ": " + parsedData[k] + "\" should be type of string");
             }
 
         }
     }
 
-    return data;
+    return parsedData;
 }
 
 module.exports = {
-    readFile: readFile
+    parseParameters: parseParameters
 };

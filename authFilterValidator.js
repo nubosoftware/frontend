6e3dcecce6f1528eb37d2c3c1c1990414391827b
Nubo Developer <dev@nubosoftware.com>
2016-06-07 18:44:15 +0300

    add permit mode to auth filter validator
    Change-Id: I25d68272828cd7a35acd5081b86861b506084a56

--- authFilterValidator.js
+++ authFilterValidator.js
@@ -1,46 +1,43 @@
 var async = require('async');
 var authFilters = require('./authFilters.js');
+var logger = require('./common.js').logger;
 
-function AuthFilterValidator(filters, excludeList){
+function AuthFilterValidator(filters, excludeList, permittedMode) {
     if (!(this instanceof AuthFilterValidator)) {
-        return new AuthFilterValidator(filters, excludeList);
+        return new AuthFilterValidator(filters, excludeList, permittedMode);
     }
 
-	this._filters = filters;
-    this._exclideList = excludeList;
-}
-
-function igonorePaths(path){
-    var resources = '/html/player/';
-
-    if(path.substring(0, resources.length) === resources){
-        return true;
-    }
-
-    return false;
+    this._filters = filters;
+    this._excludeList = excludeList;
+    this._permittedMode = permittedMode;
 }
 
 AuthFilterValidator.prototype.validate = function(req, callback) {
 
     var reqPath = req.path(req.url);
 
-    if(igonorePaths(reqPath)){
-        callback(null);
-        return;
-    }
-
     var self = this;
-        async.eachSeries(this._filters, function(filter, callback){
-            filterFunc = authFilters.getFilter(filter);
 
-            if(filterFunc){
-                filterFunc(req, self._exclideList, callback);
-            }
-             else{
-                callback("unknown filter");
-            }
-        },callback);
-    }
+    async.eachSeries(this._filters, function(filter, callback) {
+        filterFunc = authFilters.getFilter(filter);
+
+        if (filterFunc) {
+            filterFunc(req, self._excludeList, function(err) {
+                if (self._permittedMode) {
+                    if(err){
+                        logger.error("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ AuthFilterValidator: " + err + ". path: " + req.url + " (permitted mode)");
+                    }
+                    callback(null);
+                }
+                else{
+                    callback(err);
+                }
+            });
+
+        } else {
+            callback("unknown filter");
+        }
+    }, callback);
+}
 
 module.exports = AuthFilterValidator;
-

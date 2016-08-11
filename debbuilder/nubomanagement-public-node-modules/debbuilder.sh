8e83fa681c066a5cd11ed6f35609e7f5f81bad53
Nubo Developer <dev@nubosoftware.com>
2016-08-11 12:02:59 +0000

    deb build
    Change-Id: I736099e9714434e388ed2ae3123bf85307ed1ea3

---
+++ debbuilder/nubomanagement-public-node-modules/debbuilder.sh
@@ -0,0 +1,20 @@
+BUILD_ROOT=${BUILD_ROOT:="$PROJ_PATH/debbuild"}/nubomanagement-public-node-modules
+Version=${Version:="1.2.0.0"}
+
+echo "NUBO_PROJ_PATH $NUBO_PROJ_PATH"
+echo "BUILD_ROOT $BUILD_ROOT"
+
+rm -rf $BUILD_ROOT
+mkdir -p $BUILD_ROOT/opt/nubomanagement-public
+
+install -m 644 $NUBO_PROJ_PATH/nubomanagement-public/package.json $BUILD_ROOT/opt/nubomanagement-public/package.json
+
+cd $BUILD_ROOT/opt/nubomanagement-public/
+npm install
+rm package.json
+cd -
+
+
+rsync -r $PROJ_PATH/debbuilder/nubomanagement-public-node-modules/DEBIAN/ $BUILD_ROOT/DEBIAN/
+sed "s/%Version%/$Version/g" -i $BUILD_ROOT/DEBIAN/control
+

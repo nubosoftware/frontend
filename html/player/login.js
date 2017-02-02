55303f445b59cc5d3b1e4388e1405dbf5a5e4ad5
Yafim Autenshlus <yafim.autenshlus@nubosoftware.com>
2017-02-02 16:18:05 +0200

    fix - send cloneActivation only if exist
    Change-Id: I97bbc4dd10e64e6d572ca6363ff122cecfec2d4a

--- html/player/login.js
+++ html/player/login.js
@@ -842,7 +842,8 @@ $(function() {
             window.location.hash = "createPlayer";
         },
         checkActivationLink : function() {
-            var url = mgmtURL + "activationLink?token=" + encodeURIComponent(this.token) + "&cloneActivation=" + encodeURIComponent(this.cloneActivation);
+            var cloneActivationParam = this.cloneActivation ? "&cloneActivation=" + encodeURIComponent(this.cloneActivation) : "";
+            var url = mgmtURL + "activationLink?token=" + encodeURIComponent(this.token) + cloneActivationParam;
             if (DEBUG) {
                 console.log("activationLink. " + url);
             }
@@ -922,7 +923,8 @@ $(function() {
 
         },
         checkResetLink : function() {
-            var url = mgmtURL + "activationLink?token=" + encodeURIComponent(this.token) + "&cloneActivation=" + encodeURIComponent(this.cloneActivation);
+            var cloneActivationParam = this.cloneActivation ? "&cloneActivation=" + encodeURIComponent(this.cloneActivation) : "";
+            var url = mgmtURL + "activationLink?token=" + encodeURIComponent(this.token) + cloneActivationParam;
 
             if (DEBUG) {
                 console.log("checkResetLink. " + url);

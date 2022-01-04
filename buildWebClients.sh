7bab71bbb030c2822ca110ea81b9e93097ad515c
Nubo Developer <dev@nubosoftware.com>
2022-01-04 16:18:23 +0200

    Add script to build nubo-admin and nubo-desktop-client

---
+++ buildWebClients.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# build admin control panel
+if [ ! -d "./nubo-admin" ] 
+then   
+    echo "Directory nubo-admin does not exists. Clone from git project."
+    git clone git@github.com:nubosoftware/nubo-admin.git
+fi
+cd ./nubo-admin
+echo "run npm i"
+npm i
+echo "run npm un build"
+npm run build
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Error on nubo-adin build"
+    exit $retVal
+fi
+cd -
+
+#build desktop client
+if [ ! -d "./nubo-desktop-client" ] 
+then   
+    echo "Directory nubo-desktop-client does not exists. Clone from git project."
+    git clone git@github.com:nubosoftware/nubo-desktop-client.git
+fi
+cd ./nubo-desktop-client
+npm i
+npm run build
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Error on nubo-adin build"
+    exit $retVal
+fi
+cd -
+
+# copy build results to html
+rm -rf html/admin/
+cp -a ./nubo-admin/dist html/admin/
+
+rm -rf html/desktop/
+cp -a ./nubo-desktop-client/dist html/desktop/
